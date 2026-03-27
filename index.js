0c7c4e441eb03e5bfbea2dee4ef5164ec234219a
©  𝙳𝙴𝚅 𝙳𝚈𝙱𝚈 <dybydev@gmail.com>
2026-03-26 22:25:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-
-    code = require('./pair');
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/xbt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xbt.html'));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star Ladybug-MD
+  console.log(`
+Deployment Successful!
 
- Server running on http://localhost:` + PORT)
-})
+nebula-assassin-Server Running on http://localhost:` + PORT)
+});
 
-module.exports = app
+module.exports = app;
